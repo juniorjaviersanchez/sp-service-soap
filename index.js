9ddf5d99d99d8702fb1103d7a53f6009def38caf
juniorjaviersanchez <juniorsc750@gmail.com>
2026-06-05 09:19:40 -0500

    Add vehicle VAD-871

--- index.js
+++ index.js
@@ -116,7 +116,8 @@ async function enviarSOAP(ids) {
 // Ejemplo: Enviar datos para varias tablas
 const idsTablas = [
   { placa: "CAR-921", imei: "865784054739076" },
-  { placa: "F4P-710", imei: "351510090686752" }
+  { placa: "F4P-710", imei: "351510090686752" },
+  { placa: "VAD-871", imei: "867144061042246" }
 ];
 
 // Enviar cada 30 segundos
